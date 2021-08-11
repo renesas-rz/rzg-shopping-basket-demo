renesas-rz/rzg-shopping-basket-demo

commit c9ae971ed1a8b0cace281785224067a02b5924a0

Only capture frames when needed
The Demo was reading frames from the camera as a method to constantly
check if the camera is connected. This method was resource intensive and
performance limiting. Only checking the camera status when needed and
separating the functionality from the thread, which retrieves video,
provides a performance improvement.

Signed-off-by: Ayman Chaudhry <ayman.chaudhry.kc@renesas.com>

mainwindow.cpp
@@ -92,8 +92,6 @@ MainWindow::MainWindow(QWidget *parent, QString cameraLocation, QString modelLoc
             this, SLOT(receiveOutputTensor(const QVector<float>&, int, const QImage&)));
     connect(this, SIGNAL(sendNumOfInferenceThreads(int)), tfWorker, SLOT(receiveNumOfInferenceThreads(int)));
 
-    webcamTimer = new QTimer();
-
     fpsTimer = new QElapsedTimer();
 }
 
@@ -251,14 +249,14 @@ void MainWindow::on_pushButtonWebcam_clicked()
     ui->tableWidget->setRowCount(0);
     ui->labelInferenceTime->clear();
     fpsTimer->start();
+    if (ui->pushButtonWebcam->isChecked())
+        QMetaObject::invokeMethod(cvWorker, "readFrame");
 }
 
 void MainWindow::showImage(const QImage& imageToShow)
 {
-    if (ui->pushButtonWebcam->isEnabled()) {
+    if (ui->pushButtonWebcam->isEnabled())
         QMetaObject::invokeMethod(cvWorker, "readFrame");
-        webcamTimer->start();
-    }
 
     imageNew = imageToShow;
 
@@ -334,20 +332,12 @@ void MainWindow::pushButtonWebcamCheck(bool webcamButtonChecked)
 void MainWindow::webcamInitStatus(bool webcamStatus)
 {
     if (!webcamStatus) {
-        webcamTimer->stop();
-        ui->pushButtonWebcam->setEnabled(false);
-        ui->pushButtonCapture->setEnabled(false);
-        QMessageBox *msgBox = new QMessageBox(QMessageBox::Warning, "Warning", "Webcam not connected", QMessageBox::NoButton, this, Qt::Dialog | Qt::ToolTip);
-        msgBox->show();
+        webcamNotConnected();
         ui->pushButtonWebcam->setChecked(false);
     } else {
         ui->pushButtonWebcam->setEnabled(true);
         ui->pushButtonCapture->setEnabled(true);
-        QMetaObject::invokeMethod(cvWorker, "readFrame");
-        webcamTimer->setInterval(1000);
-        webcamTimer->setSingleShot(true);
-        connect(webcamTimer, SIGNAL(timeout()), this, SLOT(webcamTimeout()));
-        webcamTimer->start();
+        cvWorker->checkWebcam();
     }
 }
 
@@ -375,7 +365,7 @@ void MainWindow::on_actionReset_triggered()
     QMetaObject::invokeMethod(cvWorker, "initialiseWebcam", Qt::AutoConnection, Q_ARG(QString,webcamName));
 }
 
-void MainWindow::webcamTimeout()
+void MainWindow::webcamNotConnected()
 {
     opencvThread->deleteLater();
     ui->pushButtonWebcam->setEnabled(false);
@@ -387,12 +377,8 @@ void MainWindow::webcamTimeout()
 
 void MainWindow::on_actionDisconnect_triggered()
 {
-    webcamTimer->stop();
     QMetaObject::invokeMethod(cvWorker, "disconnectWebcam");
-    ui->pushButtonWebcam->setEnabled(false);
-    ui->pushButtonCapture->setEnabled(false);
-    QMessageBox *msgBox = new QMessageBox(QMessageBox::Warning, "Warning", "Webcam not connected", QMessageBox::NoButton, this, Qt::Dialog | Qt::ToolTip);
-    msgBox->show();
+    webcamNotConnected();
 }
 
 void MainWindow::on_actionExit_triggered()

mainwindow.h
@@ -79,7 +79,7 @@ private slots:
     void on_pushButtonWebcam_clicked();
     void on_actionLicense_triggered();
     void on_actionReset_triggered();
-    void webcamTimeout();
+    void webcamNotConnected();
 
     void on_actionDisconnect_triggered();
 
@@ -99,7 +99,6 @@ private:
     QThread *opencvThread, *tfliteThread;
     QEventLoop *qeventLoop;
     QStringList labelListSorted;
-    QTimer *webcamTimer;
     QString webcamName;
     static const QStringList labelList;
     static const std::vector<float> costs;

opencvworker.cpp
@@ -63,6 +63,16 @@ void opencvWorker::readFrame()
     emit sendImage(videoImage);
 }
 
+void opencvWorker::checkWebcam()
+{
+    if(!videoCapture->read(videoFrame)) {
+        webcamInitialised = false;
+        videoCapture->release();
+        return;
+    } else
+         webcamInitialised = true;
+}
+
 void opencvWorker::disconnectWebcam()
 {
     if (webcamInitialised == true)

opencvworker.h
@@ -36,6 +36,9 @@ class opencvWorker : public QObject
 {
     Q_OBJECT
 
+public:
+    void checkWebcam();
+
 signals:
     void sendImage(const QImage&);
     void webcamInit(bool webcamInitialised);

commit bb9271193567d14efa1f1a8c6fc67c0dd8c0806e

Fix opencvworker thread
When the application starts without a camera connected the thread is
destroyed and it crashes. By not deleting the thread when a camera is
not initialised, the application does not destroy the thread on start
up.

Fixes: c9ae971e ("Only capture frames when needed")

Signed-off-by: Ayman Chaudhry <ayman.chaudhry.kc@renesas.com>

mainwindow.cpp
@@ -367,7 +367,6 @@ void MainWindow::on_actionReset_triggered()
 
 void MainWindow::webcamNotConnected()
 {
-    opencvThread->deleteLater();
     ui->pushButtonWebcam->setEnabled(false);
     ui->pushButtonCapture->setEnabled(false);
     QMessageBox *msgBox = new QMessageBox(QMessageBox::Warning, "Warning", "Webcam not connected", QMessageBox::NoButton, this, Qt::Dialog | Qt::ToolTip);