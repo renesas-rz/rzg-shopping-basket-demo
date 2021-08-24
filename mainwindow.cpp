renesas-rz/rzg-shopping-basket-demo

commit 43db085328691e36b247e4ad3021c946e5340780

ui: Make pop-up message centered
Previously, the pop-up messages would appear at positions chosen by the
window manager. By changing the window flags being used, the pop-ups are
now always positioned in the center of the application.

Signed-off-by: Ayman Chaudhry <ayman.chaudhry.kc@renesas.com>

mainwindow.cpp
@@ -146,7 +146,7 @@ void MainWindow::on_pushButtonImage_clicked()
 void MainWindow::on_pushButtonRun_clicked()
 {
     if (!(image.depth() > 0)) {
-        QMessageBox *msgBox = new QMessageBox(QMessageBox::Warning, "Warning", "No source selected, please select an image.", QMessageBox::NoButton, this, Qt::Dialog | Qt::FramelessWindowHint);
+        QMessageBox *msgBox = new QMessageBox(QMessageBox::Warning, "Warning", "No source selected, please select an image.", QMessageBox::NoButton, this, Qt::Dialog | Qt::ToolTip);
         msgBox->show();
         return;
     }
@@ -320,7 +320,7 @@ void MainWindow::webcamInitStatus(bool webcamStatus)
         webcamTimer->stop();
         ui->pushButtonWebcam->setEnabled(false);
         ui->pushButtonCapture->setEnabled(false);
-        QMessageBox *msgBox = new QMessageBox(QMessageBox::Warning, "Warning", "Webcam not connected", QMessageBox::NoButton, this, Qt::Dialog | Qt::FramelessWindowHint);
+        QMessageBox *msgBox = new QMessageBox(QMessageBox::Warning, "Warning", "Webcam not connected", QMessageBox::NoButton, this, Qt::Dialog | Qt::ToolTip);
         msgBox->show();
         ui->pushButtonWebcam->setChecked(false);
     } else {
@@ -349,7 +349,7 @@ void MainWindow::on_actionLicense_triggered()
                              "GNU General Public License for more details.\n\n"
                              "You should have received a copy of the GNU General Public License "
                              "along with the RZG Shopping Basket Demo. If not, see https://www.gnu.org/licenses."
-                             , QMessageBox::NoButton, this, Qt::Dialog | Qt::FramelessWindowHint);
+                             , QMessageBox::NoButton, this, Qt::Dialog | Qt::ToolTip);
     msgBox->show();
 }
 
@@ -363,7 +363,7 @@ void MainWindow::webcamTimeout()
     opencvThread->deleteLater();
     ui->pushButtonWebcam->setEnabled(false);
     ui->pushButtonCapture->setEnabled(false);
-    QMessageBox *msgBox = new QMessageBox(QMessageBox::Warning, "Warning", "Webcam not connected", QMessageBox::NoButton, this, Qt::Dialog | Qt::FramelessWindowHint);
+    QMessageBox *msgBox = new QMessageBox(QMessageBox::Warning, "Warning", "Webcam not connected", QMessageBox::NoButton, this, Qt::Dialog | Qt::ToolTip);
     msgBox->show();
     ui->pushButtonWebcam->setChecked(false);
 }
@@ -374,7 +374,7 @@ void MainWindow::on_actionDisconnect_triggered()
     QMetaObject::invokeMethod(cvWorker, "disconnectWebcam");
     ui->pushButtonWebcam->setEnabled(false);
     ui->pushButtonCapture->setEnabled(false);
-    QMessageBox *msgBox = new QMessageBox(QMessageBox::Warning, "Warning", "Webcam not connected", QMessageBox::NoButton, this, Qt::Dialog | Qt::FramelessWindowHint);
+    QMessageBox *msgBox = new QMessageBox(QMessageBox::Warning, "Warning", "Webcam not connected", QMessageBox::NoButton, this, Qt::Dialog | Qt::ToolTip);
     msgBox->show();
 }
 

commit 8e8ef4e1cae368a0a7d779d9c89dfdb5fbfe662f

ui: Fix popup message
When the application is run on the new bsp without a camera attached the
wayland connection breaks and needs restarting. By replacing the window
flags, the application starts up without any wayland issues.

Fixes: 43db0853 ("ui: Make pop-up message centered")

Signed-off-by: Ayman Chaudhry <ayman.chaudhry.kc@renesas.com>

mainwindow.cpp
@@ -147,7 +147,7 @@ void MainWindow::on_pushButtonImage_clicked()
 void MainWindow::on_pushButtonRun_clicked()
 {
     if (!(image.depth() > 0)) {
-        QMessageBox *msgBox = new QMessageBox(QMessageBox::Warning, "Warning", "No source selected, please select an image.", QMessageBox::NoButton, this, Qt::Dialog | Qt::ToolTip);
+        QMessageBox *msgBox = new QMessageBox(QMessageBox::Warning, "Warning", "No source selected, please select an image.", QMessageBox::NoButton, this, Qt::Dialog | Qt::FramelessWindowHint);
         msgBox->show();
         return;
     }
@@ -356,7 +356,7 @@ void MainWindow::on_actionLicense_triggered()
                              "GNU General Public License for more details.\n\n"
                              "You should have received a copy of the GNU General Public License "
                              "along with the RZG Shopping Basket Demo. If not, see https://www.gnu.org/licenses."
-                             , QMessageBox::NoButton, this, Qt::Dialog | Qt::ToolTip);
+                             , QMessageBox::NoButton, this, Qt::Dialog | Qt::FramelessWindowHint);
     msgBox->show();
 }
 
@@ -369,7 +369,7 @@ void MainWindow::webcamNotConnected()
 {
     ui->pushButtonWebcam->setEnabled(false);
     ui->pushButtonCapture->setEnabled(false);
-    QMessageBox *msgBox = new QMessageBox(QMessageBox::Warning, "Warning", "Webcam not connected", QMessageBox::NoButton, this, Qt::Dialog | Qt::ToolTip);
+    QMessageBox *msgBox = new QMessageBox(QMessageBox::Warning, "Warning", "Webcam not connected", QMessageBox::NoButton, this, Qt::Dialog | Qt::FramelessWindowHint);
     msgBox->show();
     ui->pushButtonWebcam->setChecked(false);
 }