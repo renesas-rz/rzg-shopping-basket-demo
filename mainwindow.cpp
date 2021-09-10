renesas-rz/rzg-shopping-basket-demo

commit 9efab21cef1c1c0a0851f603ab8a941e14c2ab3e

Add ArmNN Delegate Support
In order to do this the TfLite thread must finish and be recreated with
the new delegate settings.

Signed-off-by: Gareth Williams <gareth.williams.jx@renesas.com>

mainwindow.cpp
@@ -47,6 +47,9 @@ MainWindow::MainWindow(QWidget *parent, QString cameraLocation, QString modelLoc
     , ui(new Ui::MainWindow)
 {
     webcamName = cameraLocation;
+    modelPath = modelLocation;
+    useArmNNDelegate = true;
+
     ui->setupUi(this);
     QMainWindow::showMaximized();
     scene = new QGraphicsScene(this);
@@ -89,10 +92,18 @@ MainWindow::MainWindow(QWidget *parent, QString cameraLocation, QString modelLoc
     QMetaObject::invokeMethod(cvWorker, "initialiseWebcam", Qt::AutoConnection, Q_ARG(QString, webcamName));
 
     qRegisterMetaType<QVector<float> >("QVector<float>");
+
+    createTfThread();
+
+    fpsTimer = new QElapsedTimer();
+}
+
+void MainWindow::createTfThread()
+{
     tfliteThread = new QThread();
     tfliteThread->setObjectName("tfliteThread");
     tfliteThread->start();
-    tfWorker = new tfliteWorker(modelLocation);
+    tfWorker = new tfliteWorker(modelPath, useArmNNDelegate);
     tfWorker->moveToThread(tfliteThread);
 
     connect(tfWorker, SIGNAL(requestImage()), this, SLOT(receiveRequest()));
@@ -100,8 +111,6 @@ MainWindow::MainWindow(QWidget *parent, QString cameraLocation, QString modelLoc
     connect(tfWorker, SIGNAL(sendOutputTensor(const QVector<float>&, int, const QImage&)),
             this, SLOT(receiveOutputTensor(const QVector<float>&, int, const QImage&)));
     connect(this, SIGNAL(sendNumOfInferenceThreads(int)), tfWorker, SLOT(receiveNumOfInferenceThreads(int)));
-
-    fpsTimer = new QElapsedTimer();
 }
 
 void MainWindow::on_pushButtonImage_clicked()
@@ -175,7 +184,6 @@ void MainWindow::on_inferenceThreadCount_valueChanged(int threads)
 void MainWindow::receiveRequest()
 {
     sendImage(imageToSend);
-    tfWorker->moveToThread(tfliteThread);
 }
 
 void MainWindow::receiveOutputTensor(const QVector<float>& receivedTensor, int receivedTimeElapsed, const QImage& receivedImage)
@@ -413,6 +421,30 @@ void MainWindow::on_actionDisconnect_triggered()
 
     if (!ui->checkBoxContinuous->isChecked())
         webcamNotConnected();
+}
+
+void MainWindow::on_actionEnable_ArmNN_Delegate_triggered()
+{
+    /* Update the GUI text */
+    if(useArmNNDelegate) {
+        ui->actionEnable_ArmNN_Delegate->setText("Enable ArmNN Delegate");
+        ui->labelDelegate->setText("TensorFlow Lite");
+    } else {
+        ui->actionEnable_ArmNN_Delegate->setText("Disable ArmNN Delegate");
+        ui->labelDelegate->setText("TensorFlow Lite + ArmNN delegate");
+    }
+
+    /* Toggle delegate state */
+    useArmNNDelegate = !useArmNNDelegate;
+
+    /* Remake TfLite data structures with the new ArmNNDelegate settings */
+    tfliteThread->quit();
+
+    if(!tfliteThread->wait(800)) // Allow time for the TfLite thread to finish
+        qWarning("warning: could not recreate TfLite Thread");
+
+    tfliteThread->deleteLater();
+    createTfThread();
 }
 
 void MainWindow::on_actionExit_triggered()

mainwindow.h
@@ -73,6 +73,7 @@ private slots:
     void on_pushButtonWebcam_clicked();
     void on_actionLicense_triggered();
     void on_actionReset_triggered();
+    void on_actionEnable_ArmNN_Delegate_triggered();
     void webcamNotConnected();
     void setImageSize();
     void on_actionDisconnect_triggered();
@@ -80,8 +81,10 @@ private slots:
 private:
     void drawBoxes();
     void drawFPS(qint64 timeElapsed);
+    void createTfThread();
 
     Ui::MainWindow *ui;
+    bool useArmNNDelegate;
     QPixmap image;
     QGraphicsScene *scene;
     QImage imageNew;
@@ -94,6 +97,7 @@ private:
     QEventLoop *qeventLoop;
     QStringList labelListSorted;
     QString webcamName;
+    QString modelPath;
     static const QStringList labelList;
     static const std::vector<float> costs;
     QElapsedTimer *fpsTimer;

mainwindow.ui
@@ -6,7 +6,7 @@
    <rect>
     <x>0</x>
     <y>0</y>
-    <width>1012</width>
+    <width>1612</width>
     <height>578</height>
    </rect>
   </property>
@@ -24,6 +24,69 @@
   </property>
   <widget class="QWidget" name="centralwidget">
    <layout class="QGridLayout" name="gridLayout">
+    <item row="0" column="6">
+     <widget class="QLabel" name="labelInference">
+      <property name="sizePolicy">
+       <sizepolicy hsizetype="Minimum" vsizetype="Minimum">
+        <horstretch>1</horstretch>
+        <verstretch>0</verstretch>
+       </sizepolicy>
+      </property>
+      <property name="font">
+       <font>
+        <pointsize>19</pointsize>
+       </font>
+      </property>
+      <property name="text">
+       <string>Inference Time: </string>
+      </property>
+      <property name="alignment">
+       <set>Qt::AlignLeading|Qt::AlignLeft|Qt::AlignVCenter</set>
+      </property>
+     </widget>
+    </item>
+    <item row="1" column="2">
+     <widget class="QPushButton" name="pushButtonStop">
+      <property name="sizePolicy">
+       <sizepolicy hsizetype="MinimumExpanding" vsizetype="Minimum">
+        <horstretch>1</horstretch>
+        <verstretch>0</verstretch>
+       </sizepolicy>
+      </property>
+      <property name="font">
+       <font>
+        <pointsize>17</pointsize>
+       </font>
+      </property>
+      <property name="text">
+       <string>Stop</string>
+      </property>
+     </widget>
+    </item>
+    <item row="0" column="1">
+     <widget class="QPushButton" name="pushButtonWebcam">
+      <property name="sizePolicy">
+       <sizepolicy hsizetype="MinimumExpanding" vsizetype="Minimum">
+        <horstretch>1</horstretch>
+        <verstretch>0</verstretch>
+       </sizepolicy>
+      </property>
+      <property name="font">
+       <font>
+        <pointsize>17</pointsize>
+       </font>
+      </property>
+      <property name="text">
+       <string>Load Webcam</string>
+      </property>
+      <property name="checkable">
+       <bool>true</bool>
+      </property>
+      <property name="checked">
+       <bool>false</bool>
+      </property>
+     </widget>
+    </item>
     <item row="1" column="1">
      <widget class="QPushButton" name="pushButtonCapture">
       <property name="sizePolicy">
@@ -69,106 +132,6 @@
       </property>
       <property name="value">
        <number>2</number>
-      </property>
-     </widget>
-    </item>
-    <item row="0" column="0" rowspan="2">
-     <widget class="QPushButton" name="pushButtonImage">
-      <property name="sizePolicy">
-       <sizepolicy hsizetype="MinimumExpanding" vsizetype="Minimum">
-        <horstretch>1</horstretch>
-        <verstretch>1</verstretch>
-       </sizepolicy>
-      </property>
-      <property name="font">
-       <font>
-        <pointsize>17</pointsize>
-       </font>
-      </property>
-      <property name="text">
-       <string>Load Image</string>
-      </property>
-      <property name="checkable">
-       <bool>false</bool>
-      </property>
-     </widget>
-    </item>
-    <item row="0" column="1">
-     <widget class="QPushButton" name="pushButtonWebcam">
-      <property name="sizePolicy">
-       <sizepolicy hsizetype="MinimumExpanding" vsizetype="Minimum">
-        <horstretch>1</horstretch>
-        <verstretch>0</verstretch>
-       </sizepolicy>
-      </property>
-      <property name="font">
-       <font>
-        <pointsize>17</pointsize>
-       </font>
-      </property>
-      <property name="text">
-       <string>Load Webcam</string>
-      </property>
-      <property name="checkable">
-       <bool>true</bool>
-      </property>
-      <property name="checked">
-       <bool>false</bool>
-      </property>
-     </widget>
-    </item>
-    <item row="0" column="2">
-     <widget class="QPushButton" name="pushButtonRun">
-      <property name="sizePolicy">
-       <sizepolicy hsizetype="Minimum" vsizetype="Minimum">
-        <horstretch>1</horstretch>
-        <verstretch>0</verstretch>
-       </sizepolicy>
-      </property>
-      <property name="font">
-       <font>
-        <pointsize>17</pointsize>
-       </font>
-      </property>
-      <property name="text">
-       <string>Run</string>
-      </property>
-     </widget>
-    </item>
-    <item row="1" column="2">
-     <widget class="QPushButton" name="pushButtonStop">
-      <property name="sizePolicy">
-       <sizepolicy hsizetype="MinimumExpanding" vsizetype="Minimum">
-        <horstretch>1</horstretch>
-        <verstretch>0</verstretch>
-       </sizepolicy>
-      </property>
-      <property name="font">
-       <font>
-        <pointsize>17</pointsize>
-       </font>
-      </property>
-      <property name="text">
-       <string>Stop</string>
-      </property>
-     </widget>
-    </item>
-    <item row="5" column="0" colspan="6">
-     <widget class="QGraphicsView" name="graphicsView">
-      <property name="sizePolicy">
-       <sizepolicy hsizetype="MinimumExpanding" vsizetype="Expanding">
-        <horstretch>1</horstretch>
-        <verstretch>14</verstretch>
-       </sizepolicy>
-      </property>
-      <property name="minimumSize">
-       <size>
-        <width>50</width>
-        <height>0</height>
-       </size>
-      </property>
-      <property name="sizeAdjustPolicy">
-       <enum>QAbstractScrollArea::AdjustToContents</enum>
       </property>
      </widget>
     </item>
@@ -243,24 +206,43 @@
       </property>
      </widget>
     </item>
-    <item row="0" column="6">
-     <widget class="QLabel" name="labelInference">
+    <item row="0" column="0" rowspan="2">
+     <widget class="QPushButton" name="pushButtonImage">
       <property name="sizePolicy">
-       <sizepolicy hsizetype="Minimum" vsizetype="Minimum">
+       <sizepolicy hsizetype="MinimumExpanding" vsizetype="Minimum">
         <horstretch>1</horstretch>
-        <verstretch>0</verstretch>
+        <verstretch>1</verstretch>
        </sizepolicy>
       </property>
       <property name="font">
        <font>
-        <pointsize>19</pointsize>
+        <pointsize>17</pointsize>
        </font>
       </property>
       <property name="text">
-       <string>Inference Time: </string>
+       <string>Load Image</string>
       </property>
-      <property name="alignment">
-       <set>Qt::AlignLeading|Qt::AlignLeft|Qt::AlignVCenter</set>
+      <property name="checkable">
+       <bool>false</bool>
+      </property>
+     </widget>
+    </item>
+    <item row="5" column="0" colspan="6">
+     <widget class="QGraphicsView" name="graphicsView">
+      <property name="sizePolicy">
+       <sizepolicy hsizetype="MinimumExpanding" vsizetype="Expanding">
+        <horstretch>1</horstretch>
+        <verstretch>14</verstretch>
+       </sizepolicy>
+      </property>
+      <property name="minimumSize">
+       <size>
+        <width>50</width>
+        <height>0</height>
+       </size>
+      </property>
+      <property name="sizeAdjustPolicy">
+       <enum>QAbstractScrollArea::AdjustToContents</enum>
       </property>
      </widget>
     </item>
@@ -282,6 +264,60 @@
       </property>
      </widget>
     </item>
+    <item row="0" column="2">
+     <widget class="QPushButton" name="pushButtonRun">
+      <property name="sizePolicy">
+       <sizepolicy hsizetype="Minimum" vsizetype="Minimum">
+        <horstretch>1</horstretch>
+        <verstretch>0</verstretch>
+       </sizepolicy>
+      </property>
+      <property name="font">
+       <font>
+        <pointsize>17</pointsize>
+       </font>
+      </property>
+      <property name="text">
+       <string>Run</string>
+      </property>
+     </widget>
+    </item>
+    <item row="1" column="4" colspan="2">
+     <widget class="QLabel" name="labelDelegate">
+      <property name="sizePolicy">
+       <sizepolicy hsizetype="Maximum" vsizetype="Minimum">
+        <horstretch>1</horstretch>
+        <verstretch>0</verstretch>
+       </sizepolicy>
+      </property>
+      <property name="font">
+       <font>
+        <pointsize>19</pointsize>
+       </font>
+      </property>
+      <property name="text">
+       <string>TensorFlow Lite + ArmNN delegate</string>
+      </property>
+     </widget>
+    </item>
+    <item row="0" column="4" colspan="2">
+     <widget class="QLabel" name="labelDelegate_2">
+      <property name="sizePolicy">
+       <sizepolicy hsizetype="Minimum" vsizetype="Minimum">
+        <horstretch>1</horstretch>
+        <verstretch>0</verstretch>
+       </sizepolicy>
+      </property>
+      <property name="font">
+       <font>
+        <pointsize>19</pointsize>
+       </font>
+      </property>
+      <property name="text">
+       <string>Inference Engine:</string>
+      </property>
+     </widget>
+    </item>
    </layout>
   </widget>
   <widget class="QMenuBar" name="menuBar">
@@ -289,7 +325,7 @@
     <rect>
      <x>0</x>
      <y>0</y>
-     <width>1012</width>
+     <width>1612</width>
      <height>22</height>
     </rect>
    </property>
@@ -318,8 +354,15 @@
     <addaction name="actionReset"/>
     <addaction name="actionDisconnect"/>
    </widget>
+   <widget class="QMenu" name="menuInference">
+    <property name="title">
+     <string>Inference</string>
+    </property>
+    <addaction name="actionEnable_ArmNN_Delegate"/>
+   </widget>
    <addaction name="menuAbout"/>
    <addaction name="menuCamera"/>
+   <addaction name="menuInference"/>
   </widget>
   <action name="actionLicense">
    <property name="text">
@@ -342,6 +385,17 @@
    </property>
    <property name="shortcut">
     <string>Esc</string>
+   </property>
+  </action>
+  <action name="actionEnable_ArmNN_Delegate">
+   <property name="text">
+    <string>Disable ArmNN Delegate</string>
+   </property>
+   <property name="iconText">
+    <string>ArmNN Delegate</string>
+   </property>
+   <property name="toolTip">
+    <string>ArmNN Delegate</string>
    </property>
   </action>
  </widget>

supermarket_demo_app.pro
@@ -18,7 +18,7 @@
 
 QT += core gui multimedia widgets
 
-CONFIG += c++11
+CONFIG += c++14
 
 SOURCES += \
     main.cpp \
@@ -36,7 +36,8 @@ FORMS += \
 
 INCLUDEPATH += \
     $$(SDKTARGETSYSROOT)/usr/include/opencv4 \
-    $$(SDKTARGETSYSROOT)/usr/include/tensorflow/lite/tools/make/downloads/flatbuffers/include
+    $$(SDKTARGETSYSROOT)/usr/include/tensorflow/lite/tools/make/downloads/flatbuffers/include \
+    $$(SDKTARGETSYSROOT)/usr/include/armnn \
 
 LIBS += \
     -L $$(SDKTARGETSYSROOT)/usr/lib64 \
@@ -45,3 +46,11 @@ LIBS += \
     -ltensorflow-lite \
     -ldl \
     -lutil
+
+system("echo $ARCH | grep arm64"):{
+DEFINES += SBD_ARM64
+LIBS += \
+    -larmnn \
+    -larmnnDelegate \
+    -larmnnUtils
+}

tfliteworker.cpp
@@ -22,7 +22,14 @@
 
 #include "tfliteworker.h"
 
-tfliteWorker::tfliteWorker(QString modelLocation)
+#ifdef SBD_ARM64
+#include <armnn/ArmNN.hpp>
+#include <armnn/Utils.hpp>
+#include <delegate/armnn_delegate.hpp>
+#include <delegate/DelegateOptions.hpp>
+#endif
+
+tfliteWorker::tfliteWorker(QString modelLocation, bool armnnDelegate)
 {
     tflite::ops::builtin::BuiltinOpResolver tfliteResolver;
     TfLiteIntArray *wantedDimensions;
@@ -31,6 +38,21 @@ tfliteWorker::tfliteWorker(QString modelLocation)
 
     tfliteModel = tflite::FlatBufferModel::BuildFromFile(modelLocation.toStdString().c_str());
     tflite::InterpreterBuilder(*tfliteModel, tfliteResolver) (&tfliteInterpreter);
+
+#ifdef SBD_ARM64
+    /* Setup the delegate */
+    if(armnnDelegate == true) {
+        std::vector<armnn::BackendId> backends = {armnn::Compute::CpuAcc};
+        armnnDelegate::DelegateOptions delegateOptions(backends);
+        std::unique_ptr<TfLiteDelegate, decltype(&armnnDelegate::TfLiteArmnnDelegateDelete)>
+            armnnTfLiteDelegate(armnnDelegate::TfLiteArmnnDelegateCreate(delegateOptions),
+            armnnDelegate::TfLiteArmnnDelegateDelete);
+
+        /* Instruct the Interpreter to use the armnnDelegate */
+        if (tfliteInterpreter->ModifyGraphWithDelegate(std::move(armnnTfLiteDelegate)) != kTfLiteOk)
+           qWarning("Delegate could not be used to modify the graph\n");
+    }
+#endif
 
     if (tfliteInterpreter->AllocateTensors() != kTfLiteOk)
         qFatal("Failed to allocate tensors!");

tfliteworker.h
@@ -33,7 +33,7 @@ class tfliteWorker : public QObject
     Q_OBJECT
 
 public:
-    tfliteWorker(QString modelLocation);
+    tfliteWorker(QString modelLocation, bool armnnDelegate);
 
 signals:
     void sendOutputTensor(const QVector<float>&, int, const QImage&);

commit 04d6962d77af1ca832e551138b9c875af4e468c1

inference: Do not recreate Qthread for delegate
Instead of recreating the TfLite Qthread when disabling the delegate we
should delete the existing TfLite object and recreate it without the
delegate. This prevents a potential memory leak where multiple TfLite
objects can exist.

Fixes: 9efab21cef1c

Signed-off-by: Gareth Williams <gareth.williams.jx@renesas.com>

mainwindow.cpp
@@ -94,6 +94,8 @@ MainWindow::MainWindow(QWidget *parent, QString cameraLocation, QString modelLoc
 
     qRegisterMetaType<QVector<float> >("QVector<float>");
 
+    tfliteThread = new QThread();
+    tfliteThread->setObjectName("tfliteThread");
     createTfThread();
 
     fpsTimer = new QElapsedTimer();
@@ -114,10 +116,8 @@ MainWindow::MainWindow(QWidget *parent, QString cameraLocation, QString modelLoc
 
 void MainWindow::createTfThread()
 {
-    tfliteThread = new QThread();
-    tfliteThread->setObjectName("tfliteThread");
-    tfliteThread->start();
     tfWorker = new tfliteWorker(modelPath, useArmNNDelegate);
+    tfliteThread->start();
     tfWorker->moveToThread(tfliteThread);
 
     connect(tfWorker, SIGNAL(requestImage()), this, SLOT(receiveRequest()));
@@ -464,7 +464,7 @@ void MainWindow::on_actionEnable_ArmNN_Delegate_triggered()
     if(!tfliteThread->wait(800)) // Allow time for the TfLite thread to finish
         qWarning("warning: could not recreate TfLite Thread");
 
-    tfliteThread->deleteLater();
+    delete tfWorker;
     createTfThread();
 }
 