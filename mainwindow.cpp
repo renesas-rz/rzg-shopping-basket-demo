renesas-rz/rzg-shopping-basket-demo

commit 97891a7f17b806e0ec42e77dea5a387a5adc809a

ui: Add fps counter to webcam feed
An FPS timer is added and stays active whilst the webcam feed is being
played. FPS is displayed at the top right of the camera feed on the
application.

Signed-off-by: Ayman Chaudhry <ayman.chaudhry.kc@renesas.com>

mainwindow.cpp
@@ -24,6 +24,7 @@
 #include <QEventLoop>
 #include <QTimer>
 #include <QImageReader>
+#include <QElapsedTimer>
 
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
@@ -92,6 +93,8 @@ MainWindow::MainWindow(QWidget *parent, QString cameraLocation, QString modelLoc
     connect(this, SIGNAL(sendNumOfInferenceThreads(int)), tfWorker, SLOT(receiveNumOfInferenceThreads(int)));
 
     webcamTimer = new QTimer();
+
+    fpsTimer = new QElapsedTimer();
 }
 
 void MainWindow::on_pushButtonImage_clicked()
@@ -213,6 +216,7 @@ void MainWindow::receiveOutputTensor(const QVector<float>& receivedTensor, int r
         image = QPixmap::fromImage(receivedImage);
         scene->clear();
         image.scaled(ui->graphicsView->width(), ui->graphicsView->height(), Qt::KeepAspectRatio);
+        drawFPS(fpsTimer->restart());
         scene->addPixmap(image);
         scene->setSceneRect(image.rect());
         QMetaObject::invokeMethod(tfWorker, "process");
@@ -246,6 +250,7 @@ void MainWindow::on_pushButtonWebcam_clicked()
     outputTensor.clear();
     ui->tableWidget->setRowCount(0);
     ui->labelInferenceTime->clear();
+    fpsTimer->start();
 }
 
 void MainWindow::showImage(const QImage& imageToShow)
@@ -297,6 +302,18 @@ void MainWindow::drawBoxes()
 
         scene->addRect(double(xmin), double(ymin), double(xmax - xmin), double(ymax - ymin), pen, brush);
     }
+}
+
+void MainWindow::drawFPS(qint64 timeElapsed)
+{
+    float fpsValue = 1000.0/timeElapsed;
+    QGraphicsTextItem* itemFPS = scene->addText(nullptr);
+    itemFPS->setHtml(QString("<div style='background:rgba(0, 0, 0, 100%);font-size:x-large;'>" +
+                      QString(QString::number(double(fpsValue), 'f', 1) + " FPS") +
+                      QString("</div>")));
+    itemFPS->setPos(scene->width() - X_FPS , Y_FPS);
+    itemFPS->setDefaultTextColor(TEXT_COLOUR);
+    itemFPS->setZValue(1);
 }
 
 /*

mainwindow.h
@@ -35,11 +35,15 @@
 #define X_TEXT_OFFSET 6
 #define Y_TEXT_OFFSET 18
 
+#define X_FPS 95
+#define Y_FPS 0
+
 class QGraphicsScene;
 class QGraphicsView;
 class QEventLoop;
 class opencvWorker;
 class tfliteWorker;
+class QElapsedTimer;
 
 namespace Ui { class MainWindow; } //Needed for mainwindow.ui
 
@@ -81,6 +85,7 @@ private slots:
 
 private:
     void drawBoxes();
+    void drawFPS(qint64 timeElapsed);
 
     Ui::MainWindow *ui;
     QPixmap image;
@@ -98,6 +103,7 @@ private:
     QString webcamName;
     static const QStringList labelList;
     static const std::vector<float> costs;
+    QElapsedTimer *fpsTimer;
 };
 
 #endif // MAINWINDOW_H

commit 2e7cba41e7c6693104022f41c488ffa1d0c365d7

ui: Start FPS Timer on load camera
By calculating FPS when the frame is displayed on
the GUI, the counter displays an accurate live FPS value that is
updated after each frame.

Fixes: 97891a7f17b806e0ec42e77dea5a387a5adc809a

Signed-off-by: Ayman Chaudhry <ayman.chaudhry.kc@renesas.com>

mainwindow.cpp
@@ -258,7 +258,6 @@ void MainWindow::receiveOutputTensor(const QVector<float>& receivedTensor, int r
             image = QPixmap::fromImage(receivedImage);
             scene->clear();
             image.scaled(ui->graphicsView->width(), ui->graphicsView->height(), Qt::KeepAspectRatio);
-            drawFPS(fpsTimer->restart());
             scene->addPixmap(image);
             scene->setSceneRect(image.rect());
             QMetaObject::invokeMethod(tfWorker, "process");
@@ -311,8 +310,10 @@ void MainWindow::showImage(const QImage& imageToShow)
     if ((imageNew.width() != imageWidth || imageNew.height() != imageHeight) && imageNew.depth() > 0)
         imageNew = imageNew.scaled(imageWidth, imageHeight);
 
-    if (ui->pushButtonWebcam->isChecked())
+    if (ui->pushButtonWebcam->isChecked()) {
         imageToSend = imageNew;
+        drawFPS(fpsTimer->restart());
+    }
 
     if (ui->pushButtonWebcam->isChecked() && !ui->checkBoxContinuous->isChecked()) {
         image = QPixmap::fromImage(imageToSend);