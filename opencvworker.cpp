renesas-rz/rzg-shopping-basket-demo

commit 6f949dc2dda66136c046841be92c3869c982b310

webcam: Set resolution for USB cameras
The application was setting the same resolution for all cameras. This
method caused cameras to select the closest resolution. By setting
1280x720 for USB cameras, they are using the same compatible resolution.
Signed-off-by: Ayman Chaudhry <ayman.chaudhry.kc@renesas.com>

opencvworker.cpp
@@ -31,6 +31,7 @@
 
 opencvWorker::opencvWorker(QString cameraLocation)
 {
+    int cameraHeight;
     webcamName = cameraLocation.toStdString();
     connectionAttempts = 0;
 
@@ -42,6 +43,8 @@ opencvWorker::opencvWorker(QString cameraLocation)
         /* We need to run this command only once */
         if (runCommand(cameraInitialization, stdoutput))
             qWarning("Cannot initialize the camera");
+
+        cameraHeight = 960;
     }
 
     connectCamera();

commit cb26d216ce0e22fa9ca097269f65b53bdb707d32

webcam: Fix camera resolution
When using a USB camera, a resolution of 1280x960 was being used. By
extending the scope of the camera height variable, a resolution of
1280x720 can be used for USB cameras.

Fixes: 6f949dc2 ("webcam: Set resolution for USB cameras")

Signed-off-by: Ayman Chaudhry <ayman.chaudhry.kc@renesas.com>

opencvworker.cpp
@@ -31,7 +31,6 @@
 
 opencvWorker::opencvWorker(QString cameraLocation)
 {
-    int cameraHeight;
     webcamName = cameraLocation.toStdString();
     connectionAttempts = 0;
 
@@ -43,8 +42,6 @@ opencvWorker::opencvWorker(QString cameraLocation)
         /* We need to run this command only once */
         if (runCommand(cameraInitialization, stdoutput))
             qWarning("Cannot initialize the camera");
-
-        cameraHeight = 960;
     }
 
     connectCamera();
@@ -129,6 +126,7 @@ void opencvWorker::setupCamera()
 void opencvWorker::connectCamera()
 {
     connectionAttempts++;
+    int cameraHeight = 960;
 
     /* Define the format for the camera to use */
     camera = new cv::VideoCapture(webcamName);
@@ -142,13 +140,14 @@ void opencvWorker::connectCamera()
         webcamOpened = true;
     }
 
-    camera->set(cv::CAP_PROP_FRAME_WIDTH, 1280);
-    camera->set(cv::CAP_PROP_FRAME_HEIGHT, 960);
-
     if (!usingMipi) {
         camera->set(cv::CAP_PROP_FPS, 10);
         camera->set(cv::CAP_PROP_BUFFERSIZE, 1);
+        cameraHeight = 720;
     }
+
+    camera->set(cv::CAP_PROP_FRAME_WIDTH, 1280);
+    camera->set(cv::CAP_PROP_FRAME_HEIGHT, cameraHeight);
 
     checkCamera();
 }